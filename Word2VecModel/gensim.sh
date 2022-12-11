e3000c279406833c06046e9293fc4bcc2c41dd5c
玥語ほしぞら <92282870+YueyuHoshizora@users.noreply.github.com>
2022-12-11 09:58:47 +0800

    Update gensim.sh

--- Word2VecModel/gensim.sh
+++ Word2VecModel/gensim.sh
@@ -4,7 +4,8 @@ apt install opencc -y
 apt install python3-pip -y
 pip3 install -U gensim
 pip3 install -U jieba
-axel -n 3 https://dumps.wikimedia.org/zhwiki/latest/zhwiki-latest-pages-articles.xml.bz2
+rm -f zhwiki-latest-pages-articles.xml.bz2* wiki_texts.txt wiki_zh_tw.txt s2tw.json
+axel -n 3 -o zhwiki-latest-pages-articles.xml.bz2 https://dumps.wikimedia.org/zhwiki/latest/zhwiki-latest-pages-articles.xml.bz2
 python3 wiki_to_txt.py zhwiki-latest-pages-articles.xml.bz2
 opencc -i wiki_texts.txt -o wiki_zh_tw.txt -c s2tw.json
 python3 segment.py
