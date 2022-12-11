14d32d6b85a3563242def18fef23931d41beab8e
Yueyu Hoshizora <yueyuhoshizora@gmail.com>
2022-12-11 09:28:23 +0800

    init

---
+++ Word2VecModel/README.md
@@ -0,0 +1,14 @@
+# Word2Vec Mode
+向量化NLP中文語意資料
+
+## 資料來源
+[Wiki](https://dumps.wikimedia.org/zhwiki/latest/zhwiki-latest-pages-articles.xml.bz2) `01-Dec-2022 18:10` `2.5GB`
+
+## 最後更新
+2022-12-11 08:00 UTC+8
+
+## 參考
+https://github.com/zake7749/word2vec-tutorial
+
+## License
+[Apache License, Version 2.0](https://opensource.org/licenses/Apache-2.0)
---
+++ Word2VecModel/gensim.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt install axel -y
+apt install opencc -y
+apt install python3-pip -y
+pip3 install -U gensim
+pip3 install -U jieba
+axel -n 5 https://dumps.wikimedia.org/zhwiki/latest/zhwiki-latest-pages-articles.xml.bz2
+python3 wiki_to_txt.py zhwiki-latest-pages-articles.xml.bz2
+opencc -i wiki_texts.txt -o wiki_zh_tw.txt -c s2tw.json
+python3 segment.py
+python3 train.py
+python3 demo.py
